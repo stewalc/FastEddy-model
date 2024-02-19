ca99a89b14eee5c69b93cd7b9759f559017c1927
stewartl <Luke.C.Stewart@jpl.nasa.gov>
2024-02-18 21:10:46 -0700

    cmake prototype

--- SRC/HYDRO_CORE/CUDA/cuda_buoyancyDevice.cu
+++ SRC/HYDRO_CORE/CUDA/cuda_buoyancyDevice.cu
@@ -15,6 +15,7 @@
 */
 
 #include <cuda_buoyancyDevice_cu.h>
+#include <cuda_hydroCoreDevice_cu.h>
 
 /*---BUOYANCY*/
 __constant__ int buoyancySelector_d;          /*buoyancy Force selector: 0=off, 1=on*/
