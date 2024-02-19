d92ac86344b5b5c74d083184c078865756815d17
stewartl <Luke.C.Stewart@jpl.nasa.gov>
2024-02-19 00:26:26 -0700

    cmake prototype

--- SRC/HYDRO_CORE/CUDA/cuda_moistureDevice_cu.h
+++ SRC/HYDRO_CORE/CUDA/cuda_moistureDevice_cu.h
@@ -15,7 +15,12 @@
 */
 #ifndef _MOISTURE_CUDADEV_CU_H
 #define _MOISTURE_CUDADEV_CU_H
-
+#include <hydro_core.h>
+#include <fempi.h>
+#include <grid.h>
+#include <cuda_gridDevice_cu.h>
+#include <cuda_hydroCoreDevice_cu.h>
+#include <fecuda_Utils_cu.h>
 /*moisture_ return codes */
 #define CUDA_MOISTURE_SUCCESS    0
 
